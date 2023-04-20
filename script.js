fbf380926a74fadf63d5973d836de3bfe816a2d8
joe-bor <kimborlongan_08@yahoo.com>
2023-04-20 16:44:29 -0700

    Adds ƒ init() and ƒ render()

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 /*----- constants -----*/
+const MARKS = {
+  0: "",
+  1: "X",
+  "-1": "O",
+};
 
 /*----- state variables -----*/
+let turn;
+let winner;
+let grid;
 
 /*----- cached elements  -----*/
 
 /*----- event listeners -----*/
 
 /*----- functions -----*/
+init();
+
+function init() {
+  turn = 1;
+  winner = null;
+  grid = [
+    [1, -1, 0],
+    [0, 0, 0],
+    [0, -1, 1],
+  ];
+
+  render();
+}
+
+function render() {
+  grid.forEach((rowArr, rowIdx) => {
+    rowArr.forEach((col, colIdx) => {
+      //dynamically generates id to select our div
+      const boxId = `c${rowIdx}r${colIdx}`;
+      const box = document.querySelector(`#${boxId}`);
+      box.textContent = MARKS[col];
+    });
+  });
+}
