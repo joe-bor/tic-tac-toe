18413c96342ca4fa1f0836cc331dc21335d5924d
joe-bor <kimborlongan_08@yahoo.com>
2023-04-20 17:39:04 -0700

    Updates CONSTANTS & Merges render functions

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 /*----- constants -----*/
 const MARKS = {
   0: "",
-  1: "X",
-  "-1": "O",
+  1: { marker: "🥶", color: "red" },
+  "-1": { marker: "🥵", color: "purple" },
+  //TODO: turn value to an object, to add corresponding color
 };
 
 /*----- state variables -----*/
@@ -11,7 +12,8 @@ let winner;
 let grid;
 
 /*----- cached elements  -----*/
-
+const messageEl = document.querySelector("h4");
+const playBtn = document.querySelector("button");
 /*----- event listeners -----*/
 
 /*----- functions -----*/
@@ -20,6 +22,7 @@ init();
 function init() {
   turn = 1;
   winner = null;
+  //TODO: Make sure to reset grid later after tests
   grid = [
     [1, -1, 0],
     [0, 0, 0],
@@ -28,14 +31,43 @@ function init() {
 
   render();
 }
-
+//TODO: merge the render functions
 function render() {
+  renderGrid();
+  renderMessage();
+  renderControl();
+}
+
+function renderGrid() {
   grid.forEach((rowArr, rowIdx) => {
     rowArr.forEach((col, colIdx) => {
-      //dynamically generates id to select our div
+      //dynamically generates id to select our div with their id
       const boxId = `c${rowIdx}r${colIdx}`;
       const box = document.querySelector(`#${boxId}`);
-      box.textContent = MARKS[col];
+      box.textContent = MARKS[col]["marker"];
+      box.style.color = MARKS[col]["color"];
     });
   });
 }
+
+function renderMessage() {
+  //change the message depending on state of game
+  //is there a winner? who's turn is it?
+  if (winner === "Tie") {
+    messageEl.innerText = "It's a TIE!";
+  } else if (winner) {
+    messageEl.innerHTML = `Winner: <span style="color:${MARKS[turn]["color"]}"> PLAYER ${MARKS[turn]["marker"]}</span>`;
+  } else {
+    messageEl.innerText = `Player ${MARKS[turn]}'s is thinking`;
+  }
+}
+
+function renderControl() {
+  //conditionally render the play button
+  playBtn.style.visibility = winner ? "visible" : "hidden";
+
+  //prevent re-click of div? add className?
+}
+
+//TODO: create APP's logic (clicking div)
+//TODO: create and layout winning conditions
